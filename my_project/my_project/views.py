fa2ef58d6a1aa81eb0a817bd75fb46dfb28c8664
Ahmad Hussain <official.ahmadrandhawa@gmail.com>
2025-02-07 16:07:49 +0500

    Adding home/ and about/ url views

--- my_project/my_project/views.py
+++ my_project/my_project/views.py
@@ -3,10 +3,7 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'website/index.html') # it will look for index.html in templates folder
-
-def contact(request):
-    return HttpResponse("Hello, This is contact page")  # it will return this string
+    return render(request, 'website/home.html') # it will look for home.html in templates folder
 
 def about(request):
     return HttpResponse("Hello, This is about page")
